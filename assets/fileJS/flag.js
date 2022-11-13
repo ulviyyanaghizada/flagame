aad26158b1a4909d2e063b101495474d2b4fd8cb
UlviyyaNagh <ulviyyan2002@gmail.com>
2022-11-13 20:52:33 +0400

    flag game

---
+++ assets/fileJS/flag.js
@@ -0,0 +1,58 @@
+let flagsAndCountry={}
+let newFlag;
+let count=0
+function setUpFlags(){ 
+    fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => {
+
+        for(let i=0; i<data.length;i++){
+            let flag = data[i].flags.png
+            let country =data[i].name.common
+
+            flagsAndCountry[flag] = country
+        }
+        console.log(flagsAndCountry)
+        getRandomCountry()
+    })
+}
+function getRandomCountry(){
+    const keys = Object.keys(flagsAndCountry);
+        newFlag= keys[Math.floor(Math.random() * keys.length)];
+        document.getElementById("flag").src=newFlag
+} 
+function compareUserInput(){
+        let userInput = (document.getElementById("country").value).toLowerCase()
+
+        let answer = (flagsAndCountry[newFlag]).toLowerCase()
+
+        let isCorrect = userInput ==answer
+ 
+        console.log(userInput,answer,userInput == answer)
+
+        let correct=document.querySelector(".correct")
+        let score = document.querySelector(".score")
+    if(isCorrect){
+        getRandomCountry()
+        document.getElementById("country").value = ""
+        count++
+        score.innerHTML=`<h4>score : ${count}</h4>`
+    }
+    else {
+        getRandomCountry()
+        correct.innerHTML = `<button<h4>correct answer:</h4> ${answer}</button>`
+    }
+}
+setUpFlags()
+
+let time = 20;
+var timer = setInterval(function () {
+    time--;
+    document.getElementById("timecount").innerHTML = `<h4> time: ${time}</h4>`;
+    if (time <= 0){
+    document.getElementById("timecount").innerHTML = `<h4> time is up!</h4>`;
+    // document.querySelector(".image")
+    document.querySelector(".form").innerHTML= `<h3>game is over!</h3>`
+}
+}, 1000);
+
